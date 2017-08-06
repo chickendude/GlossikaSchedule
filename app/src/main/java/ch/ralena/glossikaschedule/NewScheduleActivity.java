8ab0a8c56fc279ac3c946964505625096376bace
chickendude <pokemaster103@gmail.com>
2017-08-06 10:53:11 -0400

    Add circle indicator to new schedule view pager.

--- app/src/main/java/ch/ralena/glossikaschedule/NewScheduleActivity.java
+++ app/src/main/java/ch/ralena/glossikaschedule/NewScheduleActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import ch.ralena.glossikaschedule.fragment.NewScheduleFragment;
 import ch.ralena.glossikaschedule.fragment.NewScheduleLanguageFragment;
@@ -20,13 +23,9 @@ public class NewScheduleActivity extends AppCompatActivity implements NewSchedul
 	private static final String NEW_SCHEDULE_FRAGMENT_TAG = "new_schedule_fragment";
 	public static final String TAG_SCHEDULE_ID = "schedule_id";
 
-//	DrawerLayout drawerLayout;
 	private FragmentManager fragmentManager;
-//	NavigationAdapter navigationAdapter;
-//	ActionBarDrawerToggle drawerToggle;
-
-//	RealmResults<Schedule> schedules;
-//	Schedule loadedSchedule;
+	private ViewPager viewPager;
+	private ImageView[] circleIndicatorImages = new ImageView[3];
 
 	private Realm realm;
 
@@ -42,7 +41,22 @@ public class NewScheduleActivity extends AppCompatActivity implements NewSchedul
 //		schedules = realm.where(Schedule.class).findAll();
 
 		// set up viewpager
-		ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+		viewPager = (ViewPager) findViewById(R.id.viewPager);
+		viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+			}
+
+			@Override
+			public void onPageSelected(int position) {
+				updateCircleIndicators(position);
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int state) {
+
+			}
+		});
 		viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 			@Override
 			public int getCount() {
@@ -61,7 +75,30 @@ public class NewScheduleActivity extends AppCompatActivity implements NewSchedul
 				}
 			}
 		});
+		createCircleIndicators();
+		updateCircleIndicators(0);
 	}
+
+	private void createCircleIndicators() {
+		LinearLayout circleLayout = (LinearLayout) findViewById(R.id.viewPagerCircles);
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		layoutParams.setMargins(5, 0, 5, 0);
+		for (int i = 0; i < circleIndicatorImages.length; i++) {
+			circleIndicatorImages[i] = new ImageView(this);
+			circleIndicatorImages[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.circle_unselected));
+			circleLayout.addView(circleIndicatorImages[i], layoutParams);
+		}
+	}
+
+	private void updateCircleIndicators(int position) {
+		for (int i = 0; i < circleIndicatorImages.length; i++) {
+			circleIndicatorImages[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.circle_unselected));
+		}
+		circleIndicatorImages[position].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.circle_selected));
+	}
+
 
 	@Override
 	public void onScheduleCreated() {
